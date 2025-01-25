951a8399f82328f3f884083630fd3974064d9b15
Cyrus987 <tfish3029@gmail.com>
2025-01-25 20:06:40 +0000

    code is not working, but i think im almost there

--- InsertionSort.java
+++ InsertionSort.java
@@ -1,6 +1,32 @@
+import java.util.Arrays;
+
 public class InsertionSort implements Sorter {
 
     public void sort(int[] input) {
-        System.out.println("Insertion Sort!!!");
+        InsertionSort(input, input.length);
     }
-}
+
+     /**
+     *  Description of the Method
+     *
+     * @param  list  reference to an array of integers to be sorted
+     */
+    public void InsertionSort(int [] list, int n){
+        if (n < 2)
+        {
+            return;
+        }
+        int i,j,temp;
+        for(i=1;i<list.length;i++){
+            j = i-1; 
+            while(i>0 && list[j] > list[i]){
+                temp = list[i];
+                list[i] = list[j];
+                list[j] = temp;
+            }
+        }
+        System.out.println("insetion sorting!!!");
+    }
+    
+    }
+
